969fa6c1c91b9298e912962a4b0a6963dd8af536
cmar01 <reyalexmendoza@gmail.com>
2020-10-04 16:34:07 -0600

    cambio 6
    ultimos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-<<<<<<< Updated upstream
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireModule } from '@angular/fire';
-import { environment } from 'src/environments/environment';
-=======
-import { AppRoutingModule } from './app-routing.module';
 
 // firebase
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AppRoutingModule } from './app-routing.module';
 
 // components
 import { ProductsComponent } from './components/products/products.component';
@@ -28,29 +23,22 @@ import { ProductService } from './services/product.service';
 // Toastr, para notificaciones en angular
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< Updated upstream
-    NavbarComponent
-=======
+    NavbarComponent,
     ProductsComponent,
     ProductListComponent,
     ProductComponent,
     //NavbarComponent
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< Updated upstream
     ReactiveFormsModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule,
-=======
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
     AngularFireDatabaseModule,
     FormsModule,
     ToastrModule.forRoot(),
@@ -58,7 +46,6 @@ import { ToastrModule } from 'ngx-toastr';
   ],
   providers: [
     ProductService
->>>>>>> Stashed changes
   ],
   bootstrap: [AppComponent]
 })
